32ab7ea365bcbbb06677a7eefd280fa7da868cd9
GabrielVictor159 <gabrielvictor159487@gmail.com>
2023-02-16 01:11:53 -0300

    tentando fazer o deploy no heroky

--- server.js
+++ server.js
@@ -5,5 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000
 
 app.use(express.static('dist'))
-
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/dist/index.html');
+});
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
